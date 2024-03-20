52261e8e0ce934c4c8a8f8615276d42dc0d07da6
Ana Beatriz <anabeatrizcorreiaribeiro010@gmail.com>
2024-03-20 14:23:54 -0300

    index ana

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="resources/css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <header><img src="resources/img/lg.png" alt="logo" class="logo">
+<a>xxxxxxxxxxxxxxxxxxxxxxxx</a>
+<button>logue-já!</button>
+<button>Crie sua conta</button>
+    </header>
+    <section>
+    <a>xxxxxxxxxxxxxxxxxxxxxxxx</a>
+</section>
+
+<footer>
+
+<a>xxxxxxxxxxxxxxxxxxxxxxxx</a>
+</footer>
+</body>
+</html>
