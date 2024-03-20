1294168e66519513dd5f7f818823ca29beb208ca
Ana Beatriz <anabeatrizcorreiaribeiro010@gmail.com>
2024-03-20 15:44:12 -0300

    feito por hj

--- index.php
+++ index.php
@@ -3,17 +3,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="mind_saudavel/resources/css">
+    <link rel="stylesheet" href="mind_saudavel/resources/css/index.css">
     <title>Document</title>
 </head>
 <body>
-    <header><img src="resources/img/lg.png" alt="logo" class="logo">
-<a>xxxxxxxxxxxxxxxxxxxxxxxx</a>
-<button>logue-já!</button>
-<button>Crie sua conta</button>
+
+    <header><img src="mind_saudavel/resources/img/lg.png" alt="logo" class="logo">
+
+<div class="bt">
+    <button class="button">logue-já!</button>
+    <button class="button">Crie sua conta</button>
+</div>
+
     </header>
-    <section>
-    <a>xxxxxxxxxxxxxxxxxxxxxxxx</a>
+
+<section>
+   <div class="container">
+    <a>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</a>
+   </div>
 </section>
 
 <footer>
