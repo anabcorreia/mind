305b7fcd9999b8035645432616de13851bd19bcc
steffandrade <stefanyandrade,gomes@gmail.com>
2024-04-03 11:00:23 -0300

    ...

--- Login/app/model/model.php
+++ Login/app/model/model.php
@@ -8,12 +8,10 @@ class UserModel {
 
     // Model para criar User   
     public function criarUser($nome, $nome_u, $email, $senha) {
-       
             $sql = "INSERT INTO user (nome, nome_u, email, senha, tipo) VALUES (?, ?, ?, ?, ?)";
             $stmt = $this->pdo->prepare($sql);
             $stmt->execute([$nome, $nome_u, $email, $senha, 2]);
             return true;
-          
         } 
     }
 
